6f646fab8d234e4109cc758575634bb0abed85f9
gjmfs <mufeesmc28@gmail.com>
2025-01-27 17:19:19 +0530

    all done

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-    $server="localhost";
-    $dbusername="root";
-    $dbpassword="root";
-    $dbName="lms";
+    $server="sql309.infinityfree.com";
+    $dbusername="if0_38186783";
+    $dbpassword="YdEHo7nqi356XLa";
+    $dbName="if0_38186783_lms";
 
     $connection=mysqli_connect($server,$dbusername,$dbpassword,$dbName);
     if(!$connection){
